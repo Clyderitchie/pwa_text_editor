5a26699bdbf7301b2cfc0fc81dcbc397cddea54e
francis <clyderitchie@yahoo.com>
2024-01-10 13:09:06 -0800

    Added in the GET and PUT methods

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,10 +12,29 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// TODO: Add logic to a method that accepts some content and adds it to the database 
+// PUT
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add(content);
+  const result = await request;
+  console.error('putDb not implemented');
+  console.log('Data saved to database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// GET
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.error('getDb not implemented');
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
